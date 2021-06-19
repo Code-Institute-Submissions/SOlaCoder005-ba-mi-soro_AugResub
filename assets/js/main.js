77c1d3fa80cd3adec487abd701f2b6afb478dc1d
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-19 16:41:54 +0100

    amended form content on the contac us page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E]
+
+const ham = document.getElementById('ham');
+const navMU = document.getElementById('nav-MU');
+
+ham.addEventListener('click', () => {
+    navMU.classList.toggle('show');
+});
