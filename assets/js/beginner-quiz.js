4045e815362450e28a19b57f71b7eab70a95d4a4
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-23 13:39:31 +0100

    moved begginer-quiz js code to a seperate file
    updated the relative .html file

---
+++ assets/js/beginner-quiz.js
@@ -0,0 +1,3 @@
+const questionbl = document.getElementById("beginnerquest");
+const optionsbl = Array.from (document.getElementsByClassName("option-btn")); 
+// console.log(optionsbl);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-const questionbl = document.getElementById("beginnerquest");
-const choicesbl = Array.from (document.getElementsByClassName("option-btn")); 
-// console.log(choicesbl);
-
-//Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E]
+/**
+ * Website's Nav bar
+ * Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E] 
+*/
 const ham = document.getElementById('ham');
 const navMU = document.getElementById('nav-MU');
 
@@ -10,5 +9,7 @@ ham.addEventListener('click', () => {
     navMU.classList.toggle('show');
 });
 
-const question = document.getElementById("beginnerquest");
+
+
+
 
