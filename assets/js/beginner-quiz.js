543f827854041ad00fe73fa72a12dcba46c2544a
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-23 15:26:26 +0100

    enabled the questions to appear on quiz, using listOfQuestions, startQuiz and getNewQuestion functions

--- assets/js/beginner-quiz.js
+++ assets/js/beginner-quiz.js
@@ -1,3 +1,69 @@
-const questionbl = document.getElementById('beginnerqs');
-const optionsbl = document.getElementsByClassName('option-btn');
-console.log(questionbl);
+const question = document.getElementById("beginnerqs");
+const options = Array.from(document.getElementsByClassName("optionsbl"));
+// console.log(options);
+
+let liveQuestion = {}; //object
+let monitoringAnswers = true; //creates a delay 
+let tally = 0; 
+let questionblCounter = 0;
+let directoryQuestions = [];
+
+let listOfQuestions = [
+    {
+        question:"fgvgftgtbg",
+        option1:"test",
+        option2:"test",
+        option3:"test",
+        option4:"test",
+        answer: 1
+    },
+    {
+        question:"fgvgftgtbg",
+        option1:"test",
+        option2:"test",
+        option3:"test",
+        option4:"test",
+        answer: 3
+    },
+    {
+        question:"fgvgftgtbg",
+        option1:"test",
+        option2:"test",
+        option3:"test",
+        option4:"test",
+        answer: 4
+    },
+    {
+        question:"fgvgftgtbg",
+        option1:"test",
+        option2:"test",
+        option3:"test",
+        option4:"test",
+        answer: 2
+    },
+];
+
+startQuiz = () => {
+    questionblCounter = 0;
+    score = 0;
+    directoryQuestions = [...listOfQuestions];
+    console.log(directoryQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionblCounter++;
+    const questionIndex = Math.floor(Math.random() * directoryQuestions.length);//this allow the reandom selection of answers
+    liveQuestion = directoryQuestions[questionIndex];
+    question.innerText = liveQuestion.question;
+
+    options.forEach( option => {
+        const number = option.dataset['number'];
+        option.innerText = liveQuestion['option' + number];
+    })
+}
+
+
+
+
+startQuiz();//to start game
