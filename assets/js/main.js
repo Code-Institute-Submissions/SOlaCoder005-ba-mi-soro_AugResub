81c53ec9dc657578e4f56740a61fafdd3d6d621f
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-25 13:32:41 +0100

    moved fade out animation to main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-/**
- *Nav bar collapse and expand feature. Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E] 
-*/
+//Nav bar collapse and expand feature. Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E] 
 const ham = document.getElementById('ham');
 const navMU = document.getElementById('nav-MU');
 
@@ -8,8 +6,15 @@ ham.addEventListener('click', () => {
     navMU.classList.toggle('show');
 });
 
+//Quiz-end fade out feature. Reference material: Traversy Media - Youtuber[https://www.youtube.com/watch?v=kVc_XfZY0vI&t=69s]
+$(document).ready(function(){
+    $('#btnSeePoints').click(function(){
+        $('#gamefin').fadeOut(500);
+    });
+});
 
 
+   
 
 
 
