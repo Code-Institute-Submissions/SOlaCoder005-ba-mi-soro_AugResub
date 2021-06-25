2dacc7a3bf45fee15712e6faff8e3471484d641f
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-25 13:59:35 +0100

    added a play again  button to the quiz-end page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,3 @@ const navMU = document.getElementById('nav-MU');
 ham.addEventListener('click', () => {
     navMU.classList.toggle('show');
 });
-
-//Quiz-end fade out feature. Reference material: Traversy Media - Youtuber[https://www.youtube.com/watch?v=kVc_XfZY0vI&t=69s]
-$(document).ready(function(){
-    $('#btnSeePoints').click(function(){
-        $('#gamefin').fadeOut(500);
-    });
-});
-
-
-   
-
-
-
-
