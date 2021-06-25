215f69293c752721e7bb457357c2f709f6febeae
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-25 11:20:14 +0100

    adjusted header and footer colours
    adjusted ham icon placement on smaller screen sizes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 /**
- * Website's Nav bar
- * Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E] 
+ *Nav bar collapse and expand feature. Reference material: Florin Pop - Youtuber[https://www.youtube.com/watch?v=ydZc17rlR5E] 
 */
 const ham = document.getElementById('ham');
 const navMU = document.getElementById('nav-MU');
