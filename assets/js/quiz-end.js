222a1c6f022490674d74531fc96ffe7c81353c95
SOlaCoder005 <80408484+SOlaCoder005@users.noreply.github.com>
2021-06-25 17:32:16 +0100

    amended media queries property values to fix quiz display on smaller screens

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -1,7 +1,7 @@
 //Quiz-end fade out feature. Reference material: Traversy Media - Youtuber[https://www.youtube.com/watch?v=kVc_XfZY0vI&t=69s]
 $(document).ready(function(){
     $('#btnSeePoints').click(function(){
-        $('#gamefin').fadeOut(500);
+        $('#pointsDisplay').fadeIn(1000);
     });
 });
 
